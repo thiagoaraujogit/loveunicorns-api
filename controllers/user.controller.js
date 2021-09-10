24241d87a7da0bb264bfbdb7fcb692640ae9229f
thiagoaraujogit <thiagocerq@outlook.com>
2021-09-10 01:11:23 -0300

    feat: :zap: add findAll & findById methods

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,27 @@ const User = require('../models/user.model')
 exports.store = async (req, res) => {
   const { name, email, password } = req.body
 
-  const user = await User.create({ name, email, password })
+  try {
+    const user = await User.create({ name, email, password })
+    return res.json(user)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-  return res.json(user)
+exports.findAll = async (req, res) => {
+  const users = await User.findAll()
+
+  return res.json(users)
+}
+
+exports.findById = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findByPk(id)
+    return res.json(user)
+  } catch (error) {
+    console.log(error)
+  }
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/user.controller')
 const router = express.Router()
 
 router.post('/users', userController.store)
+router.get('/users', userController.findAll)
+router.get('/users/:id', userController.findById)
 
 module.exports = router
