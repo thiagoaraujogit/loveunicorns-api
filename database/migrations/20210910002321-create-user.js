80b4b214ee53af4704ea4d1f02c87cd6568f7495
thiagoaraujogit <thiagocerq@outlook.com>
2021-09-10 00:43:26 -0300

    fix: :fire: add freezeTableName sequelize

--- database/migrations/20210910002321-create-user.js
+++ database/migrations/20210910002321-create-user.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false
       }
-    })
+    }, { freezeTableName: true })
   },
 
   down: async (queryInterface, Sequelize) => {
